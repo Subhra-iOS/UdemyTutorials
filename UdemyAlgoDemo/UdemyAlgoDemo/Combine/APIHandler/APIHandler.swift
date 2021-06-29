68b30456adef826d18d69c89e032ae118fe58bb4
Subhra Roy <subhra.roy@e-arc.com>
2021-06-29 12:33:51 +0530

    update API Service class

--- UdemyAlgoDemo/UdemyAlgoDemo/Combine/APIHandler/APIHandler.swift
+++ UdemyAlgoDemo/UdemyAlgoDemo/Combine/APIHandler/APIHandler.swift
@@ -14,15 +14,32 @@ enum ServiceError: Error{
     case none
 }
 
+enum RemoteResponse<T, ServiceError>{
+    case output(T)
+    case fail(ServiceError)
+}
+
 class APIHandler {
     static let shared = APIHandler()
     private init(){ }
     
     func fetchRemoteCompanies() -> Future<[String], ServiceError>{
         return Future{ promise in
-            DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 3) {
-                promise(.success(["Apple", "Google", "Mocrosoft", "Facebook"]))
+            ServiceManager.fetchDataFromBackendOn { (result) in
+                switch result{
+                    case .output(let list): promise(.success(list ?? []))
+                    case .fail(let error): promise(.failure(error))
+                }
             }
+        }
+    }
+}
+
+struct ServiceManager {
+    static func fetchDataFromBackendOn(complete: @escaping (RemoteResponse<[String]?, ServiceError>) -> Void){
+        DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 3){
+            complete(.output(["Apple", "Google", "Mocrosoft", "Facebook"]))
+            //complete(.fail(.none))
         }
     }
 }
